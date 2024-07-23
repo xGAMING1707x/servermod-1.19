1bc3e5b4e96b9f3359e9cdf8bfc57d3e24e50afd
Julian Einzinger <julian5einzinger@gmail.com>
2024-07-23 20:56:45 +0200

    Added main RecipeType File

---
+++ src/main/java/net/einzinger/servermod/recipe/ModRecipes.java
@@ -0,0 +1,22 @@
+package net.einzinger.servermod.recipe;
+
+import net.einzinger.servermod.ServerMod;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModRecipes {
+    public static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, ServerMod.MOD_ID);
+
+
+    public static final RegistryObject<RecipeSerializer<ZincStationRecipe>> ZINC_PROCESSING_SERIALIZER =
+            SERIALIZERS.register("zinc_processing", () -> ZincStationRecipe.Serializer.INSTANCE);
+
+
+    public static void register(IEventBus eventBus){
+        SERIALIZERS.register(eventBus);
+    }
+}
